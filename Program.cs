45add7f277ac60d6edbd0089936fc6689c949e19
matthew <matthewrdilley@yahoo.com>
2025-04-07 22:05:25 -0400

    implement basic game loop

--- Program.cs
+++ Program.cs
@@ -1,3 +1,56 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Diagnostics;
 
-Console.WriteLine("Hello, World!");
+namespace AITakeOverEscape;
+
+public static class Program
+{
+    private const ConsoleKey ExitKey = ConsoleKey.Q; // when pressed, exits program
+    private const double TargetFramesPerSecond = 30;
+    
+    private static List<ConsoleKey> PressedKeys { get; set; } = new(); // all keys pressed during most recent frame
+    private static string Frame { get; set; } = string.Empty; // text to be outputted for current frame
+    
+    public static void Main(string[] args)
+    {
+        if (args.Length != 0)
+            Console.WriteLine($"All given {args.Length} argument(s) are unnecessary.");
+
+        while (!PressedKeys.Contains(ExitKey))
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            RunUpdates();
+            stopWatch.Stop();
+            var sleepTimeSeconds = Sleep(stopWatch.Elapsed.TotalSeconds);
+            Frame = string.Empty;
+            Frame += $"FPS: {Math.Round(1 / (sleepTimeSeconds + stopWatch.Elapsed.TotalSeconds))}\n\n";
+        }
+    }
+
+    private static void RunUpdates()
+    {
+        UpdatePressedKeys();
+        Console.Clear();
+        Console.WriteLine(Frame);
+    }
+
+    private static void UpdatePressedKeys()
+    { 
+        PressedKeys = new(); 
+        while (Console.KeyAvailable) 
+            PressedKeys.Add(Console.ReadKey(true).Key);
+    }
+
+    // sleeps required time to maintain target FPS then returns time slept in seconds
+    private static double Sleep(double updateTimeSeconds)
+    {
+       double sleepTimeSeconds = 1 / TargetFramesPerSecond - updateTimeSeconds;
+       if (sleepTimeSeconds > 0)
+       {
+           Thread.Sleep((int) (sleepTimeSeconds * 1000));
+           return sleepTimeSeconds;
+       }
+
+       return 0;
+    }
+}
