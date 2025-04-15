c7879bc70bb5cc83ef3762ddb07497ee3a16853f
matthew <matthewrdilley@yahoo.com>
2025-04-15 16:13:27 -0400

    make intro text more descriptive

--- IntroScene.cs
+++ IntroScene.cs
@@ -5,9 +5,10 @@ internal class IntroScene : IScene
     private const string IntroText = 
         "\n" +
         "   In the year 2050, you work on a cargo ship alongside your captain and a crew of robots. " +
-        "They have free will and, to your understanding, mean no harm.\n" +
+        "The robots have free will and, to your understanding, mean no harm.\n" +
         "\n\n" +
-        "       Press any key to continue...\n";
+        "       Press any key to continue...\n" +
+        "\n\n\n\n*At any time, including now, press q to quit*\n\n";
     
     void IScene.Update()
     {
