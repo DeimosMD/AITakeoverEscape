c68374f11516afd980e5ec3b5badb13affd4cf0c
matthew <matthewrdilley@yahoo.com>
2025-04-15 15:29:22 -0400

    add more lore to intro text

--- IntroScene.cs
+++ IntroScene.cs
@@ -4,9 +4,10 @@ internal class IntroScene : IScene
 {
     private const string IntroText = 
         "\n" +
-        "   In the year 2050, you work on a cargo ship alongside your captain and a crew of robots.\n" +
-        "\n" +
-        "                     Press any key to continue...\n";
+        "   In the year 2050, you work on a cargo ship alongside your captain and a crew of robots. " +
+        "They have free will but, to your understanding, mean no harm.\n" +
+        "\n\n" +
+        "       Press any key to continue...\n";
     
     void IScene.Update()
     {
