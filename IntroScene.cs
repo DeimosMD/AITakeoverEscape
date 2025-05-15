02c1b8be662b7b330a322b852040b8d72761f15b
matthew <matthewrdilley@yahoo.com>
2025-05-14 20:51:20 -0400

    modify intro text slightly

--- IntroScene.cs
+++ IntroScene.cs
@@ -5,7 +5,7 @@ internal class IntroScene : IScene
     private const string IntroText = 
         "\n" +
         $"{Program.Tab}In the year 2050, you work on a cargo ship alongside your captain and a crew of robots. " +
-        "The robots have free will and, to your understanding, mean no harm.\n" +
+        "The robots have free will and, at least to your understanding, mean no harm.\n" +
         "\n\n" +
         $"{Program.Tab+Program.Tab}Press any key to continue...\n" +
         "\n\n\n\n*At any time, including now, press [Q] to quit or [R] to reset*";
